f64f5a452b1a6db26197717b1ae5fc32de626472
bohn13 <bod3171@gmail.com>
2026-06-06 23:47:20 +0200

    detached backend

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 terraform {
-    backend "local" {}
+    backend "azurerm" {
+        resource_group_name   = "tfstate"
+        storage_account_name  = "bohn13storage"
+        container_name        = "tfstate"
+        key                   = "terraform.tfstate"
+        use_oidc              = true
+    }
 }
 provider "azurerm" {
     features {}
+    use_oidc              = true
 }
