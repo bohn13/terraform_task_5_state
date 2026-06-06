76a9da480aa986ccae13bcd870f946e739414c81
bohn13 <bod3171@gmail.com>
2026-06-06 22:46:35 +0200

    Ready

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+    backend "azurerm" {
+        resource_group_name   = "tfstate"
+        storage_account_name  = "bohn13storage"
+        container_name        = "tfstate"
+        key                   = "terraform.tfstate"
+        use_oidc              = true
+    }
+}
+provider "azurerm" {
+    features {}
+}
