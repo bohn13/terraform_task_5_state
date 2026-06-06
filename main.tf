05d904c4c093f7a5f9244903c3236fd2c0a474b0
bohn13 <bod3171@gmail.com>
2026-06-06 23:47:53 +0200

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 terraform {
-    backend "azurerm" {
-        resource_group_name   = "tfstate"
-        storage_account_name  = "bohn13storage"
-        container_name        = "tfstate"
-        key                   = "terraform.tfstate"
-        use_oidc              = true
-    }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "bohn13storage"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
 }
 provider "azurerm" {
-    features {}
-    use_oidc              = true
+  features {}
+  use_oidc = true
 }
